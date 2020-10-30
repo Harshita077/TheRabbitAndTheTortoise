397fc3880e39c32549297043a6754901ac67f852
Harshita077 <harshita.shukla_cs19@gla.ac.in>
2020-10-30 16:39:40 +0530

    pause the thread for 3 sec using thread.sleep() method

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,4 +1,50 @@
 package thread.tortoise;
 
-public class Tortoise {
+import javax.swing.*;
+
+public class Tortoise  implements Runnable{
+
+    private static final int MILESTONES = 5;
+    private final Thread tortoise;
+
+    public Tortoise(){
+        this.tortoise = new Thread(this , "Tortoise");
+
+    }
+
+    public Thread getTortoise() {
+        return tortoise;
+    }
+
+    @Override
+    public void run() {
+        for (int index = 0; index <  MILESTONES;index++){
+            switch (index){
+                case 0:
+                    System.out.println("The tortoise has started from The Start Line.");
+                    break;
+                case 1:
+                    System.out.println("The tortoise has reached The Cool River");
+                    break;
+                case 2:
+                    System.out.println("The tortoise has reached The Mountain Hill");
+                    break;
+                case 3:
+                    System.out.println("The tortoise has reached The Big Oak Tree!");
+                    break;
+                case 4:
+                    System.out.println("The tortoise has reached The Fininish Line");
+                    break;
+            }
+            try {
+
+                Thread.sleep(3000L);
+            }
+            catch (InterruptedException e){
+                System.err.println("The tortoise has lost the path!");
+
+            }
+        }
+        JOptionPane.showConfirmDialog(null,"The tortoise has completed the race!!","Tortoise",JOptionPane.OK_CANCEL_OPTION);
+    }
 }
